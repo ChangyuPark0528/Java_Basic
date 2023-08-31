bb7ed5da9f1682bebdb1521cb518bd0a08b143f2
Changyu <changyu1061@naver.com>
2023-08-31 20:37:49 +0900

    0831 자바수업 상속 다형성

--- java_Basic/src/oop/encap/good/MainClass.java
+++ java_Basic/src/oop/encap/good/MainClass.java
@@ -7,11 +7,13 @@ public class MainClass {
 		MyBirth my = new MyBirth();
 		//my.year = 1992;
 		my.setYear(1992); 
-		my.setMonth(5);
-		my.setDay(28);
+		my.setMonth(66);
+		my.setDay(0);
+		
+		my.birthInfo();
 
-		System.out.printf("내 생일은 %d년 %d월 %d일 입니다.\n"
-				,my.getYear("aaa1111!"),my.getMonth(),my.getDay());
+//		System.out.printf("내 생일은 %d년 %d월 %d일 입니다.\n"
+//				,my.getYear("aaa1111!"),my.getMonth(),my.getDay());
 	}
 
 }
